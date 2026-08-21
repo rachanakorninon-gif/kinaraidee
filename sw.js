06783b4ffc0e2c1b7e443e1876cbe8be01e5ee97
rachanakorninon-gif <317968364+rachanakorninon-gif@users.noreply.github.com>
2026-08-22 01:59:30 +0700

    Refresh beta service worker cache for latest home and iOS install updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kinaraidee-beta-v5';
+const CACHE='kinaraidee-beta-v6';
 const SHELL=[
   './','./index.html','./404.html','./manifest.webmanifest','./icon.svg',
   './feedback.html','./privacy.html','./partner.html','./robots.txt','./sitemap.xml',
