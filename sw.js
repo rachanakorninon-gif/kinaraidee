a7ca994be76541af57b224c57f267843113df941
rachanakorninon-gif <317968364+rachanakorninon-gif@users.noreply.github.com>
2026-08-24 23:37:43 +0700

    chore: bump PWA cache to v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kinaraidee-beta-v15';
+const CACHE='kinaraidee-beta-v16';
 const SHELL=[
   './','./index.html','./404.html','./manifest.webmanifest','./icon.svg',
   './feedback.html','./privacy.html','./partner.html','./robots.txt','./sitemap.xml',
