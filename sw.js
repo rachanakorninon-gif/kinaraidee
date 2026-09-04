c95646f2d7d7f03f54cbe26bbe711918d28cef23
rachanakorninon-gif <317968364+rachanakorninon-gif@users.noreply.github.com>
2026-09-04 07:40:39 +0700

    feat: cache product telemetry helper

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE='kinaraidee-beta-v16';
+const CACHE='kinaraidee-beta-v17';
 const SHELL=[
   './','./index.html','./404.html','./manifest.webmanifest','./icon.svg',
   './feedback.html','./privacy.html','./partner.html','./robots.txt','./sitemap.xml',
   './data/foods-expanded.js','./data/choice-rules.js',
   './data/group-mode.js','./data/group-sync.js','./data/group-remote.js',
-  './data/member-sync.js','./data/nearby-restaurants.js','./data/pwa-install.js',
+  './data/acquisition.js','./data/product-events.js','./data/member-sync.js',
+  './data/nearby-restaurants.js','./data/pwa-install.js',
   './data/home-surprise.js','./data/history-ui.js'
 ];
 
