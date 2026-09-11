d98288933cb104b62d759ccbe9615b3da15a197e
rachanakorninon-gif <317968364+rachanakorninon-gif@users.noreply.github.com>
2026-09-11 13:43:05 +0700

    fix(pwa): cache toast accessibility controller in app shell

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const SHELL=[
   './data/foods-expanded.js','./data/choice-rules.js',
   './data/group-mode.js','./data/group-sync.js','./data/group-remote.js',
   './data/member-sync.js','./data/nearby-restaurants.js','./data/pwa-install.js',
-  './data/home-surprise.js','./data/history-ui.js'
+  './data/home-surprise.js','./data/toast-accessibility.js','./data/history-ui.js'
 ];
 
 self.addEventListener('install',event=>event.waitUntil(
